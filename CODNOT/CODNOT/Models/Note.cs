3bc48c77bf1c0e4195519a42283fa0cd75c5b315
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2026-03-27 09:04:08 +0300

    Lab_2: Инициализация моделей. Миграции в БД и тестовые данные.

--- CODNOT/CODNOT/Models/Note.cs
+++ CODNOT/CODNOT/Models/Note.cs
@@ -1,6 +1,25 @@
-﻿namespace CODNOT.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET_CodeNotes.Models;
+
+public class Note : IHasUpdatedAt
 {
-    public class User
-    {
-    }
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Заголовок заметки обязателен")]
+    [StringLength(200, ErrorMessage = "Заголовок не может быть длиннее 200 символов")]
+    public string Title { get; set; } = string.Empty;
+
+    [StringLength(40000, ErrorMessage = "Текст заметки не может быть длиннее 40000 символов")]
+    public string? Content { get; set; }
+
+    [DataType(DataType.DateTime)]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [DataType(DataType.DateTime)]
+    public DateTime? UpdatedAt { get; set; }
+
+    public ICollection<NoteTag> NoteTags { get; set; } = new List<NoteTag>();
+
 }
--- CODNOT/CODNOT/Program.cs
+++ CODNOT/CODNOT/Program.cs
@@ -1,15 +1,19 @@
+using ASP.NET_CodeNotes.Data;
+using ASP.NET_CodeNotes.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -17,11 +21,81 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+static void SeedData(IServiceProvider serviceProvider)
+{
+    using var scope = serviceProvider.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    context.Database.Migrate();
+
+    //if (context.Notes.Any() || context.Tags.Any())
+    //{
+    //    return;
+    //}
+
+    using var transaction = context.Database.BeginTransaction();
+
+    try
+    {
+        var tagAspNet = new Tag { Name = "aspnet" };
+        var tagEfCore = new Tag { Name = "efcore" };
+        var tagSprint = new Tag { Name = "sprint" };
+
+        context.Tags.AddRange(tagAspNet, tagEfCore, tagSprint);
+        context.SaveChanges();
+
+        var notes = new[]
+        {
+            new Note ()
+                {
+                    Title = "Заголовок 1",
+                    Content = "код, текст заметки 1 c тегами efcore и aspnet",
+                    CreatedAt = DateTime.UtcNow,
+
+                },
+            new Note()
+                {
+                    Title = "Заголовок 2",
+                    Content = "Код сохраненный в заметке 2 с тегом Sprint",
+                    CreatedAt = DateTime.UtcNow,
+                },
+            new Note()
+                {
+                    Title = "Заголовок 3",
+                    Content = "Код сохраненный в заметке 2 с тегом Sprint",
+                    CreatedAt = DateTime.UtcNow,
+                }
+        };
+
+        context.Notes.AddRange(notes);
+        context.SaveChanges();
+
+        var noteTags = new[]
+        {
+            new NoteTag { NoteId = notes[0].Id, TagId = tagAspNet.Id },
+            new NoteTag { NoteId = notes[0].Id, TagId = tagEfCore.Id },
+            new NoteTag { NoteId = notes[1].Id, TagId = tagSprint.Id },
+            new NoteTag { NoteId = notes[2].Id, TagId = tagSprint.Id }
+        };
+
+        context.NoteTags.AddRange(noteTags);
+        context.SaveChanges();
+
+        transaction.Commit();
+    }
+    catch
+    {
+        transaction.Rollback();
+        throw;
+    }
+}
+
+SeedData(app.Services);
 
 app.Run();
